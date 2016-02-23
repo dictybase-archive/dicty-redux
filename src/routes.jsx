3bc90dd671d933e2c0b95c2e70be9d1675b5abbe
Siddhartha Basu <biosidd@gmail.com>
2016-02-23 12:41:59 +0000

    refactor(router):Added example router configuration

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,15 @@
 import {Route} from 'react-router';
 
 const routes = (
-    // write your routes here
+    /*
+     *write your routes here
+     *for example
+     *<Route path="/" component={ App }>
+     *    <Route path="login" component={ Login } />
+     *    <Route path="home" component={ Home } />
+     *    <Route path="error" component={ Error } />
+     *</Route>
+     */
 );
 
 export default routes;
